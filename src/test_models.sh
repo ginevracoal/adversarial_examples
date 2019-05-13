256ce1d5da3bff29f9722787af766e29a38d30aa
ginevracoal <ginevracoal@gmail.com>
2019-05-13 13:27:30 +0200

    bash script for cluster

---
+++ src/test_models.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+############
+# settings #
+############
+
+# execute this script from the folder where you want to save the logs!
+
+MODEL_NAME="random_ensemble"
+RESULTS="../trained_models/"
+DATE=$(date +%Y%m%d)
+DATETIME=$(date +%Y%m%d%H%M)
+
+## clusterino
+#
+#export CUDA_VISIBLE_DEVICES=-1 # GPU
+
+## local
+source ~/virtualenvs/venv/bin/activate
+
+#################
+# model testing #
+#################
+
+mkdir -p "$RESULTS/$DATE/"
+python "${MODEL_NAME}.py" > "$RESULTS/$DATE/${MODEL_NAME}_${DATETIME}"_out.txt
+
+## remove the unnecessary infos from output text
+#sed -n '/ETA:/!p' "results/${MODEL_NAME}_out.txt" > "results/${MODEL_NAME}_out_clean.txt"
