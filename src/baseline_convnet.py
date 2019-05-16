80cf4d3a0082a54e7b519ed1c1cf22420a0d7317
ginevracoal <ginevracoal@gmail.com>
2019-05-16 11:42:22 +0200

    new syntax

--- src/baseline_convnet.py
+++ src/baseline_convnet.py
@@ -13,7 +13,7 @@ from keras.models import Model
 from keras.layers import Dense, Dropout, Flatten, Input, Conv2D, MaxPooling2D
 from utils import *
 
-SAVE = False
+SAVE = True
 TEST = True
 
 MODEL_NAME = "baseline_convnet"
@@ -81,23 +81,14 @@ def main():
     model = BaselineConvnet(input_shape=input_shape, num_classes=num_classes)
     classifier = model.load_classifier(relative_path=TRAINED_MODEL)
 
-    #model.evaluate_adversaries(classifier, x_test, y_test, method='fgsm')
-    #x_test_adv, x_test_adv_pred = model.evaluate_adversaries(classifier, x_test, y_test, method='deepfool',
-    #                                                         adversaries_path="../data/mnist_x_test_deepfool.pkl")
-
     x_test_virtual, x_test_virtual_pred = model.evaluate_adversaries(classifier, x_test, y_test, method='virtual_adversarial')
     x_test_carlini, x_test_carlini_pred = model.evaluate_adversaries(classifier, x_test, y_test, method='carlini_l2')
 
     if SAVE is True:
         #convNet.save_model(classifier=classifier, model_name=MODEL_NAME)
 
-        carlini = os.path.join(RESULTS, time.strftime('%Y-%m-%d'), "/mnist_x_test_carlini.pkl")
-        with open(carlini, 'wb') as f:
-            pkl.dump(x_test_carlini, f)
-
-        virtual = os.path.join(RESULTS, time.strftime('%Y-%m-%d'), "/mnist_x_test_virtual.pkl")
-        with open(virtual, 'wb') as f:
-            pkl.dump(x_test_virtual, f)
+        save_to_pickle(data=x_test_virtual, filename="mnist_x_test_virtual.pkl")
+        save_to_pickle(data=x_test_carlini, filename="mnist_x_test_carlini.pkl")
 
 
 if __name__ == "__main__":
