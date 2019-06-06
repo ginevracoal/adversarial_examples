34bb11272c0c12ebf729d5e1b101d3f821194772
ginevracoal <ginevracoal@gmail.com>
2019-06-06 10:45:42 +0200

    now performing test set evaluation on each class, so that I can two ensemble methods are now available + test evaluation also gives classification reports on each projection classifier + better docstrings

--- src/random_ensemble.py
+++ src/random_ensemble.py
@@ -9,11 +9,13 @@ from art.classifiers import KerasClassifier
 from baseline_convnet import BaselineConvnet
 from keras.models import load_model
 from utils import *
+from sklearn.metrics import classification_report
 
 # settings
 TEST = True
 SIZE_PROJECTION = 8
 N_PROJECTIONS = 3
+ENSEMBLE_METHOD = "mode"  # possible methods: mode, sum
 
 # defaults
 MODEL_NAME = "random_ensemble"
@@ -80,12 +82,12 @@ class RandomEnsemble(BaselineConvnet):
     def _sum_ensemble_classifier(classifiers, projected_data):
         """
         :param classifiers: list of `n_proj` different GaussianRandomProjection objects
-        :param projected_test_data: array of test data projected on the different `n_proj` training random directions
+        :param projected_data: array of test data projected on the different `n_proj` training random directions
         (`size_proj` directions for each projection)
         :return: sum of all the predicted probabilities among each class for the `n_proj` classifiers
 
         ```
-        # Predictions on the first element by each single classifier
+        Predictions on the first element by each single classifier
         predictions[:, 0] =
          [[0.04745529 0.00188083 0.01035858 0.21188359 0.00125252 0.44483757
           0.00074033 0.13916749 0.01394993 0.1284739 ]
@@ -94,33 +96,32 @@ class RandomEnsemble(BaselineConvnet):
          [0.00000004 0.00000041 0.00000277 0.00001737 0.00000067 0.00000228
           0.         0.9995009  0.00000014 0.0004754 ]]
 
-        # Ensemble prediction vector on the first element
+        Ensemble prediction vector on the first element
         summed_predictions[0] =
          [0.05004669 0.00190451 0.49150622 0.6384873  0.0012835  0.45496735
          0.0007624  1.1760187  0.04018409 0.14483918]
         """
         # compute predictions for each projection
-        predictions = np.array([classifier.predict(projected_data[i]) for i, classifier in enumerate(classifiers)])
+        proj_predictions = np.array([classifier.predict(projected_data[i]) for i, classifier in enumerate(classifiers)])
         # sum the probabilities across all predictors
-        summed_predictions = np.sum(predictions, axis=0)
-        return summed_predictions
+        predictions = np.sum(proj_predictions, axis=0)
+        return predictions
 
     @staticmethod
     def _mode_ensemble_classifier(classifiers, projected_data):
         """
         :param classifiers: list of `n_proj` different GaussianRandomProjection objects
-        :param projected_test_data: array of test data projected on the different `n_proj` training random directions
+        :param projected_data: array of test data projected on the different `n_proj` training random directions
         (`size_proj` directions for each projection)
         :return: compute the argmax of the probability vectors and then, for each points, choose the mode over all
         classes as the predicted label
 
-        Example on sample 0.
         ```
         Computing random projections.
         Input shape:  (100, 28, 28, 1)
         Projected data shape: (3, 100, 8, 8, 1)
 
-        # Predictions on the first element by each single classifier
+        Predictions on the first element by each single classifier
         predictions[:, 0] =
         [[0.09603461 0.08185963 0.03264992 0.07047556 0.2478332  0.03418195
           0.13880958 0.19712913 0.04649669 0.05452974]
@@ -134,45 +135,77 @@ class RandomEnsemble(BaselineConvnet):
          [0. 0. 0. 0. 0. 0. 0. 0. 0. 1.]
          [0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]]
 
-        # Ensemble prediction vector on the first element
+        Ensemble prediction vector on the first element
         summed_predictions[0] =
         [0.         0.         0.         0.         0.66666667 0.
          0.         0.         0.         0.33333333]
         ```
         """
+
         # compute predictions for each projection
-        predictions = np.array([classifier.predict(projected_data[i]) for i, classifier in enumerate(classifiers)])
+        proj_predictions = np.array([classifier.predict(projected_data[i]) for i, classifier in enumerate(classifiers)])
+
         # convert probability vectors into target vectors
-        argmax_predictions = np.zeros(predictions.shape)
-        for i in range(predictions.shape[0]):
-            idx = np.argmax(predictions[i], axis=-1)
-            argmax_predictions[i, np.arange(predictions.shape[1]), idx] = 1
+        argmax_predictions = np.zeros(proj_predictions.shape)
+        for i in range(proj_predictions.shape[0]):
+            idx = np.argmax(proj_predictions[i], axis=-1)
+            argmax_predictions[i, np.arange(proj_predictions.shape[1]), idx] = 1
         # sum the probabilities across all predictors
-        summed_predictions = np.sum(argmax_predictions, axis=0)
+        predictions = np.sum(argmax_predictions, axis=0)
         # normalize
-        summed_predictions = summed_predictions / summed_predictions.sum(axis=1)[:, None]
+        predictions = predictions / predictions.sum(axis=1)[:, None]
 
         # debug
         # print(predictions[:, 0])
-        # print(argmax_predictions[:, 0])
-        # print(summed_predictions[0])
+        # print(proj_predictions[:, 0])
+        # print(final_predictions[0])
 
-        return summed_predictions
+        return predictions #, proj_predictions
 
-    def predict(self, classifier, x_test):
+    def predict(self, classifiers, data, method=ENSEMBLE_METHOD, *args, **kwargs):
         """
         Compute the average prediction over the trained models.
 
-        :param classifier: list of trained classifiers over different projections
-        :param x_test: list of projected test data
-        :return:
+        :param classifiers: list of trained classifiers over different projections
+        :param data: input data
+        :param method: #todo: descrivere
+        :return: final predictions for the input data
         """
-        x_test_projected = compute_projections(x_test, random_seeds=self.random_seeds,
-                                               n_proj=self.n_proj, size_proj=self.size_proj)
-        predictions = self._mode_ensemble_classifier(classifier, x_test_projected)
+        projected_data = compute_projections(data, random_seeds=self.random_seeds,
+                                             n_proj=self.n_proj, size_proj=self.size_proj)
+        if method == 'sum':
+            predictions = self._sum_ensemble_classifier(classifiers, projected_data)
+        elif method == 'mode':
+            predictions = self._mode_ensemble_classifier(classifiers, projected_data)
+
         return predictions
 
-    def _generate_adversaries(self, classifier, x, y, method='fgsm', adversaries_path=None):
+    def evaluate_test(self, classifiers, x_test, y_test):
+        """
+        Overwrites base class evaluation method by also performing an evaluation on each projected version of the data.
+        :param classifiers: list of trained classifiers over different projections
+        :param x_test: test data
+        :param y_test: test labels
+        :return: y_test predictions
+        """
+        print("\nTesting infos:\nx_test.shape = ", x_test.shape, "\ny_test.shape = ", y_test.shape, "\n")
+
+        x_test_proj = compute_projections(x_test, random_seeds=self.random_seeds,
+                                          n_proj=self.n_proj, size_proj=self.size_proj)
+
+        y_test_pred = np.argmax(self.predict(classifiers, x_test, method=ENSEMBLE_METHOD), axis=1)
+
+        # evaluate each classifier on its projected test set
+        baseline = BaselineConvnet(input_shape=self.input_shape, num_classes=self.num_classes)
+        for i, classifier in enumerate(classifiers):
+            print("Test evaluation on projection ", self.random_seeds[i])
+            baseline.evaluate_test(classifier, x_test_proj[i], y_test)
+
+        # final classifier evaluation on the original test set
+        super(BaselineConvnet, self).evaluate_test(classifiers, x_test, y_test)
+        return y_test_pred
+
+    def _generate_adversaries(self, classifier, x, y, method='fgsm', adversaries_path=None, test=False, *args, **kwargs):
         """ Adversaries are generated on the baseline classifier """
         baseline = BaselineConvnet(input_shape=self.input_shape, num_classes=self.num_classes)
         baseline_classifier = baseline.load_classifier(TRAINED_BASELINE)
