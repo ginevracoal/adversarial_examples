bc2a455eb733984a12155c441bdac1070ba89ba4
ginevracoal <ginevracoal@gmail.com>
2019-08-15 17:41:49 +0200

    better output logs

--- src/run_models.sh
+++ src/run_models.sh
@@ -13,13 +13,13 @@ TEST="True" # if True only takes 100 samples
 ATTACK=None # supported: "fgsm, "pgd", "deepfool", "carlini_linf"
 
 # === randens === #
-N_PROJ_LIST=[6,9,12,15] # supported: lists containing 0,..,15
-		          # default for training is [15], default for testing is [6,9,12,15]
-SIZE_PROJ_LIST=[8,12,16,20] # supported: lists containing 8,12,16,20
+N_PROJ_LIST=[6,9,12,15] # Supported: lists containing 0,..,15. Default for training is [15], default for testing is [6,9,12,15]
+
+# === randens, parallel_randens === #
+SIZE_PROJ_LIST=[8,12,16,20] # Supported: list containing 8, 12, 16, 20. Default is [8 12 16 20]
 
 # === parallel_randens === #
-N_PROJ=15 # supported: 0,...,15
-SIZE_PROJ=8 # supported: 8, 12, 16, 20
+#N_PROJ=15 # default is 15
 
 # === clusterino === #
 #rm screenlog.0
@@ -36,23 +36,24 @@ TIME=$(date +%H:%M:%S)
 RESULTS="../results/$DATE/"
 mkdir -p $RESULTS
 
-OUT="${RESULTS}${DATASET_NAME}_${SCRIPT}"
+FILEPATH="${RESULTS}${TIME}_${DATASET_NAME}_${SCRIPT}"
 if [ "$TEST" = "True" ] ; then
-    OUT="${OUT}_test"
+    FILEPATH="${FILEPATH}_test"
 fi
+OUT="${FILEPATH}_out.txt"
+CLEAN_OUT="${FILEPATH}_clean.txt"
+COMPLEXITY="${FILEPATH}_complexity.txt"
 
 if [ $SCRIPT = "baseline" ]; then
-  BASELINE_OUT="${OUT}_${ATTACK}"
-  python3 "baseline_convnet.py" $DATASET_NAME $TEST $ATTACK > "${BASELINE_OUT}_out.txt"
-  sed -n '/ETA:/!p' "${BASELINE_OUT}_out.txt" > "${BASELINE_OUT}_clean.txt"
+  python3 "baseline_convnet.py" $DATASET_NAME $TEST $ATTACK > $OUT
+  sed -n '/ETA:/!p' $OUT > $CLEAN_OUT
 elif [ $SCRIPT = "randens" ]; then
-  RANDENS_OUT="${OUT}_${N_PROJ_LIST}_${SIZE_PROJ_LIST}_${ATTACK}"
-  python3 "random_ensemble.py" $DATASET_NAME $TEST $N_PROJ_LIST $SIZE_PROJ_LIST $ATTACK > "${RANDENS_OUT}_out.txt"
-  sed -n '/ETA:/!p' "${RANDENS_OUT}_out.txt"  > "${RANDENS_OUT}_clean.txt"
+  python3 "random_ensemble.py" $DATASET_NAME $TEST $N_PROJ_LIST $SIZE_PROJ_LIST $ATTACK >> $OUT
+  sed -n '/ETA:/!p' $OUT  >> $CLEAN_OUT
 elif [ $SCRIPT = "parallel_randens" ]; then
-  for proj_idx in $(seq $N_PROJ); do
-    PARALLEL_RANDENS_OUT="${OUT}_${proj_idx}_${SIZE_PROJ}"
-    python3 "parallel_randens_training.py" $DATASET_NAME $TEST $proj_idx $SIZE_PROJ > "${PARALLEL_RANDENS_OUT}_out.txt"
-    grep -e "Rand" -e "Training time for" "${PARALLEL_RANDENS_OUT}_out.txt" >> "${OUT}_${SIZE_PROJ}_complexity.txt"
+  for proj_idx in $(seq 0 15); do
+    python3 "parallel_randens_training.py" $DATASET_NAME $TEST $proj_idx $SIZE_PROJ_LIST >> $OUT
+    sed -n '/ETA:/!p' $OUT  >> $CLEAN_OUT
+    grep -e "Rand" -e "Training time for" $OUT >> $COMPLEXITY
   done
 fi
