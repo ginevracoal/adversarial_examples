0d44621cb9c722343043d7ce7a45662bc597d79b
ginevracoal <ginevracoal@gmail.com>
2019-11-07 11:27:42 +0100

    parallel compute_projections

--- src/parallel_random_ensemble.py
+++ src/parallel_random_ensemble.py
@@ -4,6 +4,7 @@ from random_ensemble import *
 from projection_functions import compute_single_projection
 from utils import load_dataset
 from joblib import Parallel, delayed
+import multiprocessing
 
 MODEL_NAME = "random_ensemble"
 PROJ_MODE = "channels"
@@ -19,6 +20,7 @@ class ParallelRandomEnsemble(RandomEnsemble):
                                                      data_format=data_format, dataset_name=dataset_name, test=test,
                                                      epochs=epochs)
         self.proj_idx = proj_idx
+        self.n_jobs = 2  # multiprocessing.cpu_count()
 
     @staticmethod
     def _set_session(device):
@@ -50,18 +52,42 @@ class ParallelRandomEnsemble(RandomEnsemble):
                                         filename=self._set_baseline_filename(seed=self.proj_idx))
         return proj_classifier
 
-    def parallel_train(self, device):
-        import multiprocessing
-        self.classifiers = Parallel(n_jobs=multiprocessing.cpu_count())(
+    def train(self, x, y, device):
+        self.classifiers = Parallel(n_jobs=self.n_jobs)(
             delayed(_parallel_train)(dataset_name=self.dataset_name, test=self.test, proj_idx=proj_idx,
                                      size_proj=self.size_proj, proj_mode=self.projection_mode, device=device)
             for proj_idx in range(self.n_proj))
         self.trained = True
 
+    def compute_projections(self, input_data):
+        """
+        Parallel implementation of this method
+        :param input_data:
+        :return:
+        """
+        projections = Parallel(n_jobs=self.n_jobs)(
+            delayed(_parallel_compute_projections)(input_data, proj_idx=proj_idx, size_proj=self.size_proj,
+                                                   projection_mode=self.projection_mode)
+            for proj_idx in self.random_seeds)
+
+        # eventually adjust input dimension to a single channel projection
+        projections = np.array(projections)
+
+        if projections.shape[4] == 1:
+            self.input_shape = (self.input_shape[0], self.input_shape[1], 1)
+        else:
+            self.input_shape = (self.input_shape[0], self.input_shape[1], 3)
+        return projections, None
+
     def _sum_ensemble_classifier(self, classifiers, projected_data):
+        """
+        Parallelized version of this method.
+        :param classifiers: list of trained classifiers
+        :param projected_data: list of projected data for all of the n_prok random initializations
+        :return:
+        """
         # compute predictions for each projection
-        import multiprocessing
-        results = Parallel(n_jobs=multiprocessing.cpu_count())(
+        results = Parallel(n_jobs=self.n_jobs)(
             delayed(_parallel_predict)(classifier=classifier, projected_data=projected_data[i], device=device)
             for i, classifier in enumerate(classifiers))
         proj_predictions = np.array(results)
@@ -69,14 +95,14 @@ class ParallelRandomEnsemble(RandomEnsemble):
         predictions = np.sum(proj_predictions, axis=0)
         return predictions
 
-    def parallel_evaluate(self, x, y):
-        """
-        Performs parallel evaluation of the ensemble model on cpu.
-        :param x: evaluation data
-        :param y: evaluation labels
-        :return: model predictions
-        """
-        super(ParallelRandomEnsemble, self).evaluate(x, y)
+    # def evaluate(self, x, y, report_projections=False):
+    #     """
+    #     Performs parallel evaluation of the ensemble model on cpu.
+    #     :param x: evaluation data
+    #     :param y: evaluation labels
+    #     :return: model predictions
+    #     """
+    #     super(ParallelRandomEnsemble, self).evaluate(x, y)
 
     # def load_classifier(self, relative_path, folder=None, filename=None):
     #     classifiers = super(ParallelRandomEnsemble, self).load_classifier(relative_path, folder, filename)
@@ -128,6 +154,12 @@ def _parallel_train(dataset_name, test, proj_idx, size_proj, proj_mode, device):
     del tf
 
 
+def  _parallel_compute_projections(input_data, proj_idx, size_proj, projection_mode):
+    projection, _ = compute_single_projection(input_data=input_data, seed=proj_idx,
+                                                               size_proj=size_proj, projection_mode=projection_mode)
+    return projection
+
+
 def main(dataset_name, test, proj_idx, n_proj, size_proj, proj_mode, device):
 
     x_train, y_train, x_test, y_test, input_shape, num_classes, data_format = load_dataset(dataset_name, test)
@@ -142,18 +174,21 @@ def main(dataset_name, test, proj_idx, n_proj, size_proj, proj_mode, device):
     model = ParallelRandomEnsemble(input_shape=input_shape, num_classes=num_classes, size_proj=size_proj,
                                    proj_idx=None, n_proj=n_proj, data_format=data_format, dataset_name=dataset_name,
                                    projection_mode=proj_mode, test=test)
-    # model.parallel_train(device=device)
+    # model.train(x_train, y_train, device=device)
     model.load_classifier(relative_path=RESULTS)
-    model.parallel_evaluate(x=x_test, y=y_test)
+    # model.load_classifier(relative_path=TRAINED_MODELS)
 
-    # deprecated:
-    # model = RandomEnsemble(input_shape=input_shape, num_classes=num_classes, size_proj=size_proj, n_proj=n_proj,
-    #                        data_format=data_format, dataset_name=dataset_name, projection_mode=proj_mode,
-    #                        test=test)
-    # model.evaluate(x_test, y_test)
-    # for attack in ['fgsm','pgd','deepfool','carlini']:
-    #     x_test_adv = model.load_adversaries(attack=attack, eps=EPS)
-    #     model.evaluate(x_test_adv, y_test)
+    # === parallel evaluate === #
+    model.evaluate(x=x_test, y=y_test)
+    for method in ['fgsm', 'pgd', 'carlini']:
+        x_test_adv = model.load_adversaries(attack=method, eps=0.3)
+        model.evaluate(x_test_adv, y_test)
+    for method in ['fgsm', 'pgd', 'carlini']:
+        x_test_adv = model.load_adversaries(attack=method, eps=0.5)
+        model.evaluate(x_test_adv, y_test)
+
+    x_test_adv = model.load_adversaries(attack="deepfool", eps=None)
+    model.evaluate(x_test_adv, y_test)
 
 
 if __name__ == "__main__":
