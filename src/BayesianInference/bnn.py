a73709356f8f97a461dc2946a5b5297739a2edce
ginevracoal <ginevracoal@gmail.com>
2020-01-07 17:31:35 +0100

    dataloaders util

--- src/BayesianInference/bnn.py
+++ src/BayesianInference/bnn.py
@@ -8,12 +8,25 @@ import torch
 import pyro
 from pyro.contrib import bnn
 from BayesianSGD.nets import torch_net
-from utils import load_dataset
+from utils import load_dataset, onehot_to_labels
 from torch.distributions import OneHotCategorical
+from torch.utils.data import DataLoader
+
+
+def data_loaders(dataset_name, batch_size, n_samples):
+    x_train, y_train, x_test, y_test, input_shape, num_classes, data_format = \
+        load_dataset(dataset_name=dataset_name, n_samples=n_samples, test=False)
+    y_train = onehot_to_labels(y_train)
+    y_test = onehot_to_labels(y_test)
+
+    train_loader = DataLoader(dataset=list(zip(x_train, y_train)), batch_size=batch_size)
+    test_loader = DataLoader(dataset=list(zip(x_test, y_test)), batch_size=batch_size)
+
+    return train_loader, test_loader, data_format, input_shape
 
 
 class BNN(nn.Module):
-    def __init__(self, dataset_name, input_shape, data_format, test):
+    def __init__(self, dataset_name, input_shape, data_format):
         super(BNN, self).__init__()
         self.net = torch_net(dataset_name=dataset_name, input_shape=input_shape, data_format=data_format)
         self.n_hidden = 1024
@@ -25,26 +38,29 @@ class BNN(nn.Module):
         # Set-up parameters for the distribution of weights for each layer `a<n>`
         a1_mean = torch.zeros(784, self.n_hidden)
         a1_scale = torch.ones(784, self.n_hidden)
+        a1_dropout = torch.tensor(0.25)
         a2_mean = torch.zeros(self.n_hidden + 1, self.n_classes)
         a2_scale = torch.ones(self.n_hidden + 1, self.n_hidden)
+        a2_dropout = torch.tensor(1.0)
         a3_mean = torch.zeros(self.n_hidden + 1, self.n_classes)
         a3_scale = torch.ones(self.n_hidden + 1, self.n_hidden)
+        a3_dropout = torch.tensor(1.0)
         a4_mean = torch.zeros(self.n_hidden + 1, self.n_classes)
         a4_scale = torch.ones(self.n_hidden + 1, self.n_classes)
         with pyro.plate('data', size=size):
             # sample conditionally independent hidden layers
-            h1 = pyro.sample('h1', bnn.HiddenLayer(flat_inputs, a1_mean, a1_scale, non_linearity=nnf.leaky_relu,
+            h1 = pyro.sample('h1', bnn.HiddenLayer(flat_inputs, a1_mean, a1_dropout*a1_scale, non_linearity=nnf.leaky_relu,
                                                    KL_factor=kl_factor))
-            h2 = pyro.sample('h2', bnn.HiddenLayer(h1, a2_mean, a2_scale, non_linearity=nnf.leaky_relu,
+            h2 = pyro.sample('h2', bnn.HiddenLayer(h1, a2_mean, a2_dropout*a2_scale, non_linearity=nnf.leaky_relu,
                                                    KL_factor=kl_factor))
-            h3 = pyro.sample('h3', bnn.HiddenLayer(h2, a3_mean, a3_scale, non_linearity=nnf.leaky_relu,
+            h3 = pyro.sample('h3', bnn.HiddenLayer(h2, a3_mean, a3_dropout*a3_scale, non_linearity=nnf.leaky_relu,
                                                    KL_factor=kl_factor))
             logits = pyro.sample('logits', bnn.HiddenLayer(h3, a4_mean, a4_scale,
                                                            non_linearity=lambda x: nnf.log_softmax(x, dim=-1),
                                                            KL_factor=kl_factor,
                                                            include_hidden_bias=False))
             # One-hot encode labels
-            #labels = nnf.one_hot(labels) if labels is not None else None # they are one hot already!
+            labels = nnf.one_hot(labels) if labels is not None else None
 
             # Condition on the observed labels
             cond_model = pyro.sample('label', OneHotCategorical(logits=logits), obs=labels)
@@ -56,22 +72,28 @@ class BNN(nn.Module):
         a1_mean = pyro.param('a1_mean', 0.01 * torch.randn(784, self.n_hidden))
         a1_scale = pyro.param('a1_scale', 0.1 * torch.ones(784, self.n_hidden),
                               constraint=constraints.greater_than(0.01))
+        a1_dropout = pyro.param('a1_dropout', torch.tensor(0.25),
+                                constraint=constraints.interval(0.1, 1.0))
         a2_mean = pyro.param('a2_mean', 0.01 * torch.randn(self.n_hidden + 1, self.n_hidden))
         a2_scale = pyro.param('a2_scale', 0.1 * torch.ones(self.n_hidden + 1, self.n_hidden),
                               constraint=constraints.greater_than(0.01))
+        a2_dropout = pyro.param('a2_dropout', torch.tensor(1.0),
+                                constraint=constraints.interval(0.1, 1.0))
         a3_mean = pyro.param('a3_mean', 0.01 * torch.randn(self.n_hidden + 1, self.n_hidden))
         a3_scale = pyro.param('a3_scale', 0.1 * torch.ones(self.n_hidden + 1, self.n_hidden),
                               constraint=constraints.greater_than(0.01))
+        a3_dropout = pyro.param('a3_dropout', torch.tensor(1.0),
+                                constraint=constraints.interval(0.1, 1.0))
         a4_mean = pyro.param('a4_mean', 0.01 * torch.randn(self.n_hidden + 1, self.n_classes))
         a4_scale = pyro.param('a4_scale', 0.1 * torch.ones(self.n_hidden + 1, self.n_classes),
                               constraint=constraints.greater_than(0.01))
 
         with pyro.plate('data', size=size):
-            h1 = pyro.sample('h1', bnn.HiddenLayer(flat_inputs, a1_mean, a1_scale, non_linearity=nnf.leaky_relu,
+            h1 = pyro.sample('h1', bnn.HiddenLayer(flat_inputs, a1_mean, a1_dropout*a1_scale, non_linearity=nnf.leaky_relu,
                                                    KL_factor=kl_factor))
-            h2 = pyro.sample('h2', bnn.HiddenLayer(h1, a2_mean, a2_scale, non_linearity=nnf.leaky_relu,
+            h2 = pyro.sample('h2', bnn.HiddenLayer(h1, a2_mean, a2_dropout*a2_scale, non_linearity=nnf.leaky_relu,
                                                    KL_factor=kl_factor))
-            h3 = pyro.sample('h3', bnn.HiddenLayer(h2, a3_mean, a3_scale, non_linearity=nnf.leaky_relu,
+            h3 = pyro.sample('h3', bnn.HiddenLayer(h2, a3_mean, a3_dropout*a3_scale, non_linearity=nnf.leaky_relu,
                                                    KL_factor=kl_factor))
             logits = pyro.sample('logits', bnn.HiddenLayer(h3, a4_mean, a4_scale,
                                                            non_linearity=lambda x: nnf.log_softmax(x, dim=-1),
@@ -95,3 +117,12 @@ class BNN(nn.Module):
         print("\nLoading params: ", filepath)
         pyro.get_param_store().load(filepath)
 
+    def evaluate_test(self, test_loader, device):
+        self.eval()
+        total = 0.0
+        correct = 0.0
+        for images, labels in test_loader:
+            pred = self.forward(images.to(device).view(-1, 784), n_samples=1)
+            total += labels.size(0)
+            correct += (pred.argmax(-1) == labels.to(device)).sum().item()
+        print(f"\nTest accuracy: {correct / total * 100:.5f}")
--- src/BayesianInference/vi_bnn.py
+++ src/BayesianInference/vi_bnn.py
@@ -5,21 +5,19 @@ import pyro
 from BayesianInference.bnn import BNN
 from pyro.infer import SVI, TraceMeanField_ELBO, Trace_ELBO
 from pyro import poutine
-from torch.utils.data import DataLoader
 from utils import *
 import pyro.optim as pyroopt
 from pyro.optim import PyroOptim
 import random
 import torch
+from BayesianInference.bnn import data_loaders
 
 
 class VI_BNN(BNN):
-    def __init__(self, dataset_name, input_shape, data_format, test):
-        super(VI_BNN, self).__init__(dataset_name=dataset_name, input_shape=input_shape, data_format=data_format,
-                                     test=test)
+    def __init__(self, dataset_name, input_shape, data_format):
+        super(VI_BNN, self).__init__(dataset_name=dataset_name, input_shape=input_shape, data_format=data_format)
 
     def infer_parameters(self, train_loader, device, lr=0.01, momentum=0.9, num_epochs=30):
-        self.train()
         print("\nSVI inference.")
         optim = pyroopt.SGD({'lr': lr, 'momentum': momentum, 'nesterov': True})
         svi = SVI(self.model, self.guide, optim, loss=Trace_ELBO())
@@ -38,9 +36,9 @@ class VI_BNN(BNN):
                 pred = self.forward(images.to(device), n_samples=1).mean(0)
                 total_loss += loss / len(train_loader.dataset)
                 total += labels.size(0)
-                correct += (pred.argmax(-1) == onehot_to_labels(labels).to(device)).sum().item()
+                correct += (pred.argmax(-1) == labels.to(device)).sum().item()
                 accuracy = correct / total * 100
-                param_store = pyro.get_param_store()
+
             print(f"[Epoch {i + 1}]\t loss: {total_loss:.2f} \t accuracy: {accuracy:.2f}")
             loss_list.append(total_loss)
             accuracy_list.append(accuracy)
@@ -55,57 +53,41 @@ class VI_BNN(BNN):
             res.append(t.nodes['logits']['value'])
         return torch.stack(res, dim=0)
 
-    def evaluate_test(self, test_loader, device):
-        self.eval()
-        total = 0.0
-        correct = 0.0
-        for images, labels in test_loader:
-            pred = self.forward(images.to(device).view(-1, 784), n_samples=1)
-            total += labels.size(0)
-            correct += (pred.argmax(-1) == onehot_to_labels(labels).to(device)).sum().item()
-        print(f"\nTest accuracy: {correct / total * 100:.5f}")
 
-
-def main(dataset_name, lr, n_epochs, device, test, seed):
+def main(dataset_name, n_samples, lr, n_epochs, device, seed):
     random.seed(seed)
     batch_size = 128
+    train_loader, test_loader, data_format, input_shape = \
+        data_loaders(dataset_name=dataset_name, batch_size=batch_size, n_samples=n_samples)
 
-    x_train, y_train, x_test, y_test, input_shape, num_classes, data_format = \
-        load_dataset(dataset_name=dataset_name, test=test)
     pyro.clear_param_store()
-    bayesnn = VI_BNN(dataset_name=dataset_name, data_format=data_format, input_shape=input_shape, test=test)
+    bayesnn = VI_BNN(dataset_name=dataset_name, data_format=data_format, input_shape=input_shape)
+    dict = bayesnn.infer_parameters(train_loader=train_loader, device=device, num_epochs=n_epochs, lr=lr)
+    bayesnn.evaluate_test(test_loader=test_loader, device=device)
 
-    train_data = DataLoader(dataset=list(zip(x_train,y_train)), batch_size=batch_size)
-
-    dict = bayesnn.infer_parameters(train_loader=train_data, device=device, num_epochs=n_epochs, lr=lr)
-
-    test_data = DataLoader(dataset=list(zip(x_test, y_test)), batch_size=batch_size)
-    bayesnn.evaluate_test(test_loader=test_data, device=device)
-
-    filename = "vi_"+str(dataset_name)+"_lr="+str(lr)+"_epochs="+str(n_epochs)+"_seed="+str(seed)
-    # bayesnn.save(filename=filename)
+    filename = "vi_"+str(dataset_name)+"_samples="+str(n_samples)+"_lr="+str(lr)+"_epochs="+str(n_epochs)+"_seed="+str(seed)
+    bayesnn.save(filename=filename)
     # bayesnn.load(filename=filename)
-    bayesnn.evaluate_test(test_loader=test_data, device=device)
+    # bayesnn.evaluate_test(test_loader=test_data, device=device)
 
-    if test is False:
-        plot_loss_accuracy(dict, path=RESULTS+"_"+filename+".png")
+    plot_loss_accuracy(dict, path=RESULTS+"bnn/"+filename+".png")
 
 
 if __name__ == "__main__":
     try:
         dataset_name = sys.argv[1]
-        lr = float(sys.argv[2])
-        n_epochs = int(sys.argv[3])
-        device = sys.argv[4]
-        test = eval(sys.argv[5])
+        n_samples = int(sys.argv[2])
+        lr = float(sys.argv[3])
+        n_epochs = int(sys.argv[4])
+        device = sys.argv[5]
         seed = int(sys.argv[6])
 
     except IndexError:
         dataset_name = input("\nChoose a dataset: ")
+        n_samples = input("\nChoose the number of samples (type=int): ")
         lr = input("\nSet the learning rate: ")
         n_epochs = input("\nSet the number of epochs: ")
         device = input("\nChoose a device (cpu/gpu): ")
-        test = input("\nDo you just want to test the code? (True/False): ")
         seed = input("\nSet a training seed (type=int): ")
 
-    main(dataset_name, lr, n_epochs, device, test, seed)
+    main(dataset_name, n_samples, lr, n_epochs, device, seed)
